209c4f82891b9d9212d50952c2d37e703b41f85e
Maxime Picaud <maxime.picaud@icasore.com>
2010-11-28 13:28:02 +0100

    [master] helper + messenger style

--- lib/helper/MelodyHelper.php
+++ lib/helper/MelodyHelper.php
@@ -2,7 +2,24 @@
 
 function social_button($service, $route, $options = array())
 {
-  $image = isset($options['button-type'])?$options['button-type']:'default';
-  $image .= '.png';
-  return link_to(image_tag('/sfMelodyPlugin/images/'.$service.'/'.$image, array('alt' => 'Connect with '.$service)), $route, $options);
+  use_stylesheet('/sfMelodyPlugin/css/button.css');
+  
+  $options = array_merge(array('button-type' => 'default', 'type' => 'image', 'class' => $service.'-button'), $options);
+
+  $content = null;
+
+  switch ($options['type'])
+  {
+    case 'text':
+      $content = $options['content'];
+      break;
+    default:
+    case 'image':
+      $image = $options['button-type'].'.png';
+      $content = image_tag('/sfMelodyPlugin/images/'.$service.'/'.$image, array('alt' => 'Connect with '.$service));
+      break;
+  }
+
+  unset($options['button-type'], $options['type'], $options['image']);
+  return link_to($content, $route, $options);
 }
